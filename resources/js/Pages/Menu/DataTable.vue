f9602ffb5aaf8a98e020479b7c75ee3966652409
Geriano <gerznewbie@gmail.com>
2022-03-06 14:31:33 +0700

    removing search menu feature

--- resources/js/Pages/Menu/DataTable.vue
+++ resources/js/Pages/Menu/DataTable.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="flex flex-col space-y-2 bg-slate-100 border border-slate-300 rounded-md shadow-xl">
-    <div class="flex-none flex items-center justify-end space-x-2 bg-slate-200 py-2 px-4">
-      <input v-model="search" type="text" class="w-full sm:w-1/3 bg-slate-100 text-xs border border-slate-300 rounded-md shadow placeholder:capitalize" :placeholder="__('search something')">
-    </div>
-
     <div class="w-full sm:w-2/3 p-4">
       <Builder :menus="menus" />
     </div>
@@ -26,12 +22,6 @@
       Link,
       Icon,
       Builder,
-    },
-
-    data() {
-      return {
-        search: new String,
-      }
     },
   })
 </script>
