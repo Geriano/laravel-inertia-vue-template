473cc1d496dac84d0dcd7316230ce8345e3c26c5
Geriano <gerznewbie@gmail.com>
2022-03-23 20:13:38 +0700

    remove some route

--- routes/web.php
+++ routes/web.php
@@ -35,32 +35,3 @@ Route::any('/icon/{file:string}/{r:int}/{g:int}/{b:int}', [App\Http\Controllers\
 Route::any('/icon/{file:string}/{r:int}/{g:int}/{b:int}/{a:float}', [App\Http\Controllers\Icon::class, 'rgba'])->name('icon.rgba');
 
 Route::get('/test', [App\Http\Controllers\Test::class, 'index']);
-
-Route::get('/mfs', function () {
-    Artisan::call('migrate:fresh', [
-        '--seed' => true,
-    ]);
-
-    return redirect()->route('login');
-});
-
-Route::get('/trc', function () {
-    Illuminate\Support\Facades\DB::table('users')->truncate();
-    Illuminate\Support\Facades\DB::table('password_resets')->truncate();
-    Illuminate\Support\Facades\DB::table('failed_jobs')->truncate();
-    Illuminate\Support\Facades\DB::table('personal_access_tokens')->truncate();
-    Illuminate\Support\Facades\DB::table('teams')->truncate();
-    Illuminate\Support\Facades\DB::table('team_user')->truncate();
-    Illuminate\Support\Facades\DB::table('team_invitations')->truncate();
-    Illuminate\Support\Facades\DB::table('sessions')->truncate();
-    Illuminate\Support\Facades\DB::table('permissions')->truncate();
-    Illuminate\Support\Facades\DB::table('roles')->truncate();
-    Illuminate\Support\Facades\DB::table('model_has_permissions')->truncate();
-    Illuminate\Support\Facades\DB::table('model_has_roles')->truncate();
-    Illuminate\Support\Facades\DB::table('role_has_permissions')->truncate();
-    Illuminate\Support\Facades\DB::table('menus')->truncate();
-    Illuminate\Support\Facades\DB::table('menu_permission')->truncate();
-    Illuminate\Support\Facades\DB::table('migrations')->truncate();
-
-    return redirect()->to(url('/mfs'));
-});
