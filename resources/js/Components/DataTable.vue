00ce4c8dd2732ca5baa707fda67784dbea07ce83
Geriano <gerznewbie@gmail.com>
2022-04-09 00:00:06 +0700

    fix bug data table per page is only 10 and moving loading state

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -1,32 +1,32 @@
 <template>
-  <transition>
-    <div v-if="loading" class="bg-slate-100 p-4 rounded-md h-auto">
-      Loading...
-    </div>
+  <div class="bg-slate-100 p-0 rounded-md">
+    <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 sm:items-center justify-between mb-2">
+      <div class="flex w-full sm:w-auto items-center space-x-2 justify-between sm:justify-start">
+        <span class="lowercase first-letter:capitalize">{{ __('per page') }}</span>
 
-    <div v-else class="bg-slate-100 p-0 rounded-md">
-      <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 sm:items-center justify-between mb-2">
-        <div class="flex w-full sm:w-auto items-center space-x-2 justify-between sm:justify-start">
-          <span class="lowercase first-letter:capitalize">{{ __('per page') }}</span>
+        <select v-model="config.perPage" @change.prevent="refresh" class="bg-transparent border border-slate-300 rounded-md p-1">
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="25">25</option>
+          <option value="50">50</option>
+          <option value="108">100</option>
+          <option value="508">500</option>
+        </select>
 
-          <select v-model="config.perPage" @change.prevent="refresh" class="bg-transparent border border-slate-300 rounded-md p-1">
-            <option value="10">10</option>
-            <option value="10">15</option>
-            <option value="10">25</option>
-            <option value="10">50</option>
-            <option value="10">100</option>
-            <option value="10">500</option>
-          </select>
-
-          <button v-if="softDeletes" @click.prevent="config.withTrashed = ! config.withTrashed; refresh()" class="border border-slate-300 rounded px-3 py-1 uppercase">
-            {{ __(config.withTrashed ? 'without trashed' : 'with trashed') }}
-          </button>
-        </div>
-
-        <input type="search" v-model="config.search" @input.prevent="refresh" class="bg-transparent sm:w-2/5 border border-slate-300 rounded-md placeholder:capitalize text-xs" :placeholder="__('search something')">
+        <button v-if="softDeletes" @click.prevent="config.withTrashed = ! config.withTrashed; refresh()" class="border border-slate-300 rounded px-3 py-1 uppercase">
+          {{ __(config.withTrashed ? 'without trashed' : 'with trashed') }}
+        </button>
       </div>
 
-      <div class="overflow-auto border border-slate-300 rounded-md mb-2 max-h-[25rem]">
+      <input type="search" v-model="config.search" @input.prevent="refresh" class="bg-transparent sm:w-2/5 border border-slate-300 rounded-md placeholder:capitalize text-xs" :placeholder="__('search something')">
+    </div>
+
+    <transition name="fade">
+      <div v-if="loading" class="flex items-center justify-center bg-slate-100 p-4 rounded-md h-[25rem]">
+        <span>Loading...</span>
+      </div>
+
+      <div v-else class="overflow-auto border border-slate-300 rounded-md mb-2 max-h-[25rem]">
         <table class="w-full border-collapse">
           <thead class="bg-slate-200 sticky top-0 z-10">
             <slot name="head" :config="config" :sort="sort" />
@@ -41,26 +41,26 @@
           </tbody>
         </table>
       </div>
+    </transition>
 
-      <div class="flex items-center">
-        <div class="flex-none w-1/4">
-          <p class="lowercase first-letter:capitalize text-xs">
-            {{ __('displaying') }} {{ data.from }} {{ __('to') }} {{ data.to }} {{ __('from') }} {{ data.total }}
-          </p>
-        </div>
+    <div class="flex items-center">
+      <div class="flex-none w-1/4">
+        <p class="lowercase first-letter:capitalize text-xs">
+          {{ __('displaying') }} {{ data.from }} {{ __('to') }} {{ data.to }} {{ __('from') }} {{ data.total }}
+        </p>
+      </div>
 
-        <div class="flex flex-wrap w-full items-center justify-end">
-          <div
-            v-for="(link, i) in data.links"
-            :key="i"
-            v-html="__(link.label)"
-            :class="(i == 0 && 'rounded-l-md') || (i == data.links.length - 1 && 'rounded-r-md') || (link.active && 'bg-slate-700 border-slate-600 text-slate-100')"
-            @click.prevent="page(link)"
-            class="text-xs border p-1 shadow cursor-pointer"></div>
-        </div>
+      <div class="flex flex-wrap w-full items-center justify-end">
+        <div
+          v-for="(link, i) in data.links"
+          :key="i"
+          v-html="__(link.label)"
+          :class="(i == 0 && 'rounded-l-md') || (i == data.links.length - 1 && 'rounded-r-md') || (link.active && 'bg-slate-700 border-slate-600 text-slate-100')"
+          @click.prevent="page(link)"
+          class="text-xs border p-1 shadow cursor-pointer"></div>
       </div>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
